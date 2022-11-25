e36b14eee34b94ac42c106259431ce39f1e3d43e
Elise <justin555@live.nl>
2022-11-25 12:06:23 +0100

    Started on a portfolio site

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Elises her sekai</title>
+</head>
+<body>
+    <h1>Website coming soon</h1>
+    <div class="centerPiece">
+        <img alt="picture about me" class="eliseImg" src="img/Elise.png">
+    </div>
+    <p class="construction"> under construction at the moment</p>
+</body>
+</html>
+
