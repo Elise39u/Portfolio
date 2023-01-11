ca9ff3b9be2230511ff7aef075eaa94cf7b80351
Elise <justin555@live.nl>
2023-01-11 11:26:29 +0100

    Updated the socials, added schedule section and a new me with updates

--- index.php
+++ index.php
@@ -9,6 +9,32 @@
     <title>Elises her sekai</title>
 </head>
 <body>
+<div class="sidebar">
+    <h1 class="mainH1"> Socials of the girl with the mark</h1>
+        <!-- https://www.svgrepo.com/ -->
+        <a href="https://www.twitch.tv/hatsune_juju">
+            <img class="twitch svgIcons" src="img/SVGIcons/twitch-svgrepo-com.svg">
+        </a>
+        <a href="https://www.instagram.com/twitch_hatsunejuju/">
+            <img class="insta svgIcons" src="img/SVGIcons/instagram-svgrepo-com.svg">
+        </a>
+        <a href="https://twitter.com/TJuju124">
+            <img class="twitter svgIcons" src="img/SVGIcons/twitter-svgrepo-com.svg">
+        </a>
+        <a href="https://www.youtube.com/channel/UCFK1FzoHQ3XlQaqejb3fGMg">
+            <img class="youtubeMain svgIcons" src="img/SVGIcons/youtube-svgrepo-com.svg">
+        </a>
+        <a href="https://www.youtube.com/channel/UCpGrqQTMjFGFecEMd7F3YcQ">
+            <img class="youtubeVods svgIcons" src="img/SVGIcons/youtube-svgrepo-com.svg">
+        </a>
+        <a href="https://steamcommunity.com/id/HatsuneJuju/">
+            <img class="steam svgIcons" src="img/SVGIcons/steam-svgrepo-com.svg">
+        </a>
+        <a href="https://discord.gg/3P9Nwya">
+            <img class="discord svgIcons" src="img/SVGIcons/discord-svgrepo-com.svg">
+        </a>
+</div>
+
 <div class="tile bottom-right">
     <h1 class="centerWelcomeText">Hello There. Welcome to my site!</h1>
     <div class="centerPiece">
@@ -28,13 +54,15 @@
                                   allowfullscreen></iframe> </p>
     <h2 class="introductionH2"> Elise is the name a miku fan of heart</h2>
     <p class="introductionText"> Hi there im Elise. I'm a huge fan of Hatsune Miku and currently studying an IT bachelor. <br>
-        Im 23 years old as of moment of typing and live on my own <br>
+        Im 24 years old as of moment of typing and live on my own <br>
         In my free time I like to be a content creator on the internet. <br>
-        I often make YouTube content and stream on twitch. I also tend to make stuff for myself <br>
-        Like for example I made a discord python bot that run on repel in my own twitch server. <br>
-        This is how I live and code my way around on the internet. It's quite an adventure. <br>
+        I often make YouTube content and stream on twitch. I do all sorts of stuff  <br>
+        But im mostly a Project Diva/Horror youtuber. On twitch i like to switch around<br>
+        With one Project Diva stream in the weekend and something else in the week <br>
+        But i also do plenty of other stuff on my channels make sure to check it out <br>
         Interested in the content I make on Youtube or twitch?. <br>
-        Let me show you my mark powers and watch the video on the page.</p>
+        Let me show you my mark powers and watch the video on the page. I also have a discord <br>
+        So if your interested to stay up to date then make sure to join that in the menu left.</p>
     <p class="introductionText"> I also like to draw and play games. I'm very creative <br>
         Something you think might be obvious with the all the content creation <br>
         I mostly like to play with friends but don't hesitate to play something on my own <br>
@@ -43,37 +71,48 @@
 </div>
 
 <div class="tile bottom-right">
-    <h1 class="mainH1"> Socials of the girl with the mark</h1>
+    <h1 class="mainH1"> Streaming and Upload Schedule </h1>
     <div class="centerPiece">
-    <!-- https://www.svgrepo.com/ -->
-    <a href="https://www.twitch.tv/hatsune_juju">
-        <img class="twitch svgIcons" src="img/SVGIcons/twitch-svgrepo-com.svg">
-    </a>
-    <a href="https://www.instagram.com/twitch_hatsunejuju/">
-        <img class="insta svgIcons" src="img/SVGIcons/instagram-svgrepo-com.svg">
-    </a>
-    <a href="https://twitter.com/TJuju124">
-        <img class="twitter svgIcons" src="img/SVGIcons/twitter-svgrepo-com.svg">
-    </a>
-    <a href="https://www.youtube.com/channel/UCFK1FzoHQ3XlQaqejb3fGMg">
-        <img class="youtubeMain svgIcons" src="img/SVGIcons/youtube-svgrepo-com.svg">
-    </a>
-    <a href="https://www.youtube.com/channel/UCpGrqQTMjFGFecEMd7F3YcQ">
-        <img class="youtubeVods svgIcons" src="img/SVGIcons/youtube-svgrepo-com.svg">
-    </a>
-    <a href="https://steamcommunity.com/id/HatsuneJuju/">
-        <img class="steam svgIcons" src="img/SVGIcons/steam-svgrepo-com.svg">
-    </a>
-    <a href="https://discord.gg/3P9Nwya">
-        <img class="discord svgIcons" src="img/SVGIcons/discord-svgrepo-com.svg">
-    </a>
+        <img alt="picture about me" class="eliseImg" src="img/Elise2.png">
     </div>
+    <h2 class="introductionH2"> Youtube </h2>
+    <p class="introductionText"> Schedule for uploads and stream..... oh that is good a one. <br>
+    Should we start with the YouTube then. As i said earlier i'm an Project Diva/Horror youtuber <br>
+    But i also do lots of other stuff on the channel. But question remains how much do i upload then? <br>
+        I aim for 3x uploads a week with a extra horror vid on friday. But the list is as follows <br> </p>
+    <ul>
+        <li>1x Random game Video</li>
+        <li>1x Project diva Video (Has priority)</li>
+        <li>1x Random game Video and 3 scary games video on friday</li>
+    </ul>
+    <p class="introductionText">
+    I still aiming to keep a Horror friday with 2 uploads each time. <br>
+        Also 3 scary games is my only real consistent thing on the channel. <br>
+        I upload that every friday or try that to around 20:00 CET/CEST <br>
+        Besides that you can expect at least 1 project diva video in the week <br>
+        But as i said i also do plenty of other stuff make sure to check the video out on the page here!
+    </p>
+    <h2 class="introductionH2"> Twitch </h2>
+    <p class="introductionText"> So now comes the question twitch he.... Well let me tell you <br>
+    Back in March 2022 i made the decision to go on a weekly schedule. <br>
+    This primarily with the reason of a upcoming Internship semester and my of course new Transgender life <br>
+    So once a week on sunday i make a schedule for upcoming week and post it. <br>
+    You can find the schedule then on the following places </p>
+    <ul>
+        <li><b>In my Discords Schedule channel</b> Can be found under the Socials category</li>
+        <li><b>My twitter.</b> i also post announcements here if i go live on the day</li>
+        <li><b>My Instagram.</b> i also post announcements here if i go live on the day</li>
+        <li><b>Vods Youtube channel.</b> Good place if you wanna see how stream goes</li>
+    </ul>
+    <p class="introductionText"> All these socials can be found in the menu of the left <br>
+    With the red YouTube icon being my Vods YouTube channel <br>
+    Do i catch you maby live once on twitch in the future <br>
+    I would love to see you there don't be afraid to say hi then. Greeting Elise </p>
 </div>
-</body>
 
 <div class="tile bottom-right">
-    <h1 class="mainH1"> Streaming and Upload Schedule </h1>
-    <p class="centerWelcomeText pEle"> Coming in the next update</p>
+    <h1 class="mainH1"> Covers Artist and Og songs  </h1>
+    <p class="centerWelcomeText pEle"> Coming soon in the next update </p>
 </div>
 </body>
 
